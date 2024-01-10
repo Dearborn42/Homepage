f43ad0134a1cd1ecbddac504171e93a254671d5c
Dearborn42 <amurf26@outlook.com>
2024-01-10 14:51:24 -0700

    Initial commit

---
+++ homepage/src/App.js
@@ -0,0 +1,11 @@
+import Acordian from "./Components/Acordian";
+
+function App() {
+  return (
+    <div className="App">
+      <Acordian />
+    </div>
+  );
+}
+
+export default App;
---
+++ homepage/src/Components/Acordian.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+import Question from './Question';
+import "../Acordian_Styles/main.css"
+import data from "../Utils/data.js"
+
+const Acordian = () => {
+  const [questions, setQuestions] = useState(data)
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and Answers about logins</h3>
+        <section className='info'>
+          {questions.map(x => (<Question key={x.id} {...x} />))}
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default Acordian
