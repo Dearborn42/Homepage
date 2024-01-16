35406a9da8d2426fa942b4b026ecd26680477a9b
Dearborn42 <amurf26@outlook.com>
2024-01-15 21:59:47 -0700

    added reactive spofity

--- homepage/src/Components/Menu.jsx
+++ homepage/src/Components/Menu.jsx
@@ -15,6 +15,7 @@ const Menu = ({items}) => {
   return (
     <div className="menu">
         {sortedCategories.map(x => (<Acordian category={x} data={sorted[x]} />))}
+        <iframe style={{borderRadius: `12px`}} src="https://open.spotify.com/embed/playlist/0p68nRIH0Bw9UlHFJdU44s?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </div>
   )
 }
